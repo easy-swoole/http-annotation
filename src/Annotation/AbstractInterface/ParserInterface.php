486443308e6be592908c82cee8dc6044cfea6502
如果的如果 <admin@fosuss.com>
2020-06-29 15:32:43 +0800

    ApiGroup*实现

--- src/Annotation/AbstractInterface/ParserInterface.php
+++ src/Annotation/AbstractInterface/ParserInterface.php
@@ -9,9 +9,5 @@ use EasySwoole\HttpAnnotation\Annotation\ObjectAnnotation;
 
 interface ParserInterface
 {
-    function cache():CacheInterface;
-
-    public function getAnnotationParser(): Annotation;
-
     function getObjectAnnotation(string $class, ?int $filterType = null): ObjectAnnotation;
 }
