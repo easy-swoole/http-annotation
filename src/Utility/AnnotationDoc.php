7150af6d1658db3d01ead843bb0b01eb27a6d09d
如果的如果 <admin@fosuss.com>
2020-08-10 15:21:39 +0800

    完善注解 api path mapping

--- src/Utility/AnnotationDoc.php
+++ src/Utility/AnnotationDoc.php
@@ -4,6 +4,8 @@
 namespace EasySwoole\HttpAnnotation\Utility;
 
 
+use EasySwoole\HttpAnnotation\Annotation\MethodAnnotation;
+use EasySwoole\HttpAnnotation\Annotation\ObjectAnnotation;
 use EasySwoole\HttpAnnotation\Annotation\ParserInterface;
 
 class AnnotationDoc
@@ -15,8 +17,20 @@ class AnnotationDoc
         $this->scanner = new Scanner($parser);
     }
 
-    function render($dirOrFile)
+    function render($dirOrFile):string
     {
+        $groupList = [];
+        $result = '';
         $list = $this->scanner->scanAnnotations($dirOrFile);
+        /** @var ObjectAnnotation $objectAnnotation */
+        foreach ($list as $objectAnnotation)
+        {
+            if($objectAnnotation->getApiGroupTag()){
+                $group = $objectAnnotation->getApiGroupTag()->groupName;
+            }else{
+                $group = 'default';
+            }
+            $groupList[$group] = [];
+        }
     }
 }
