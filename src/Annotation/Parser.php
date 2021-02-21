9649d693bef3f50128460dfeeae805add8e5a692
如果的如果 <admin@fosuss.com>
2021-02-21 14:09:11 +0800

    支持从父类获取注解

--- src/Annotation/Parser.php
+++ src/Annotation/Parser.php
@@ -5,6 +5,7 @@ namespace EasySwoole\HttpAnnotation\Annotation;
 
 
 use EasySwoole\Annotation\Annotation;
+use EasySwoole\HttpAnnotation\AnnotationController;
 use EasySwoole\HttpAnnotation\AnnotationTag\Api;
 use EasySwoole\HttpAnnotation\AnnotationTag\ApiAuth;
 use EasySwoole\HttpAnnotation\AnnotationTag\ApiDescription;
@@ -73,17 +74,49 @@ class Parser implements ParserInterface
 
     function parseObject(\ReflectionClass $reflectionClass): ObjectAnnotation
     {
+        $parent = $reflectionClass->getParentClass();
+        if($parent && $parent->getName() !== AnnotationController::class){
+            if(Cache::getInstance()->get($parent->getName())){
+                $parent = Cache::getInstance()->get($parent->getName());
+            }else{
+                $parent = $this->parseObject($parent);
+            }
+        }
         $cache  = Cache::getInstance()->get($reflectionClass->getName());
         if($cache){
             return $cache;
         }
         $objectAnnotation = new ObjectAnnotation();
 
+        if($parent instanceof ObjectAnnotation){
+            if($parent->getApiGroupTag()){
+                $objectAnnotation->addAnnotationTag($parent->getApiGroupTag());
+            }
+            if($parent->getApiGroupDescriptionTag()){
+                $objectAnnotation->addAnnotationTag($parent->getApiGroupDescriptionTag());
+            }
+            foreach ($parent->getGroupAuthTag() as $tag){
+                $objectAnnotation->addAnnotationTag($tag);
+            }
+            foreach ($parent->getParamTag() as $tag){
+                $objectAnnotation->addAnnotationTag($tag);
+            }
+            foreach ($parent->getOtherTags() as $otherTag){
+                $objectAnnotation->addAnnotationTag($otherTag);
+            }
+        }
+        //获取class的标签注解
         $tagList = $this->parser->getAnnotation($reflectionClass);
         array_walk_recursive($tagList,function ($item)use($objectAnnotation){
             $objectAnnotation->addAnnotationTag($item);
         });
 
+        //获取class的方法注解
+        if($parent instanceof ObjectAnnotation){
+            foreach ($parent->getMethod() as $method){
+                $objectAnnotation->addMethod($method);
+            }
+        }
         foreach ($reflectionClass->getMethods()  as $method){
             $tagList = $this->parser->getAnnotation($method);
             $method = new MethodAnnotation($method->getName());
@@ -93,6 +126,12 @@ class Parser implements ParserInterface
             $objectAnnotation->addMethod($method);
         }
 
+        //获取class的成员属性注解
+        if($parent instanceof ObjectAnnotation){
+            foreach ($parent->getProperty() as $property){
+                $objectAnnotation->addProperty($property);
+            }
+        }
         foreach ($reflectionClass->getProperties() as $property)
         {
             $tagList = $this->parser->getAnnotation($property);
