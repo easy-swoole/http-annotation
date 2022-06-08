7131a5823d548da822d045e062097619068e1a7e
如果的如果 <admin@fosuss.com>
2022-06-08 20:02:00 +0800

    delete

--- src/AnnotationController.php
+++ src/AnnotationController.php
@@ -13,38 +13,55 @@ abstract class AnnotationController extends Controller
 {
     public function __hook(?string $actionName, Request $request, Response $response)
     {
-        $this->runAnnotationHook($actionName);
+        $this->runAnnotationHook($actionName,$request);
         return parent::__hook($actionName, $request, $response);
     }
 
-    private function runAnnotationHook(string $method)
+    private function runAnnotationHook(string $method,Request $request)
     {
-        $ref = ReflectionCache::getInstance()->getClassReflection(static::class);
-        $onRequestParams = $ref->getMethod("onRequest")->getAttributes(Param::class);
 
+        $actionParams = AttributeCache::getInstance()->getClassMethodMap(static::class,$method);
+        if($actionParams === null){
+            $actionParams = [];
+            $ref = ReflectionCache::getInstance()->getClassReflection(static::class);
+            $onRequestParams = $ref->getMethod("onRequest")->getAttributes(Param::class);
 
-        $actionParams = [];
-        if($ref->hasMethod($method)){
-            $actionMethod = $ref->getMethod($method);
-            $actionApiTags = $actionMethod->getAttributes(Api::class);
-            if(!empty($actionApiTags)){
-                /** @var \ReflectionAttribute $actionApiTag */
-                $actionApiTag = $actionApiTags[0];
-                $actionParams = $actionApiTag->getArguments()['params'];
+            if($ref->hasMethod($method)){
+                $actionMethod = $ref->getMethod($method);
+                $actionApiTags = $actionMethod->getAttributes(Api::class);
+                if(!empty($actionApiTags)){
+                    /** @var \ReflectionAttribute $actionApiTag */
+                    $actionApiTag = $actionApiTags[0];
+                    $actionParams = $actionApiTag->getArguments()['params'];
+                }
+            }
+
+            foreach ($onRequestParams as $onRequestParam){
+                $args = $onRequestParam->getArguments();
+                $onRequestParam = new Param(...$args);
+
+                $hit = false;
+                foreach ($actionParams as $actionParam){
+                    if($actionParam->name == $onRequestParam->name){
+                        $hit = true;
+                        break;
+                    }
+                }
+                if(!$hit){
+                    $actionParams[] = $onRequestParam;
+                }
+            }
+
+            $allowMethod = ReflectionCache::getInstance()->allowMethodReflections($ref);
+            //如果是存在于用户定义的方法，才允许缓存起来。
+            if(isset($allowMethod[$method])){
+                AttributeCache::getInstance()->setClassMethodMap(static::class,$method,$actionParams);
             }
         }
 
-        foreach ($onRequestParams as $onRequestParam){
-            $hit = false;
-            foreach ($actionParams as $actionParam){
-                if($actionParam->name == $onRequestParam->getArguments()['name']){
-                    $hit = true;
-                    break;
-                }
-            }
-            if(!$hit){
-                $actionParams[] = $onRequestParam->getArguments();
-            }
+        /** @var Param $param */
+        foreach ($actionParams as $param){
+            $param->parsedValue($request);
         }
     }
 }
--- src/AttributeCache.php
+++ src/AttributeCache.php
@@ -10,7 +10,7 @@ class AttributeCache
     use Singleton;
 
     protected array $apiGroup = [];
-    protected array $methodAnnotation = [];
+    protected array $classMethodMap = [];
 
     function addApiGroup(ApiGroup $apiGroup):bool
     {
@@ -25,5 +25,24 @@ class AttributeCache
     function apiGroups():array
     {
         return $this->apiGroup;
+    }
+
+    function setClassMethodMap(string $class,$action,array $data):void
+    {
+        $key = md5($class);
+        $this->classMethodMap[$key][$action] = $data;
+    }
+
+    function getClassMethodMap(string $class,$action):?array
+    {
+        $key = md5($class);
+        if(isset($this->classMethodMap[$key][$action])){
+            $list = [];
+            foreach ($this->classMethodMap[$key][$action] as $item){
+                $list[] = clone $item;
+            }
+            return $list;
+        }
+        return null;
     }
 }
