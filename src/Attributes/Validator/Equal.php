e9a90c85cfec3a270f2ec68f4c347e9e5687b09b
如果的如果 <admin@fosuss.com>
2022-06-10 10:25:16 +0800

    适配

---
+++ src/Attributes/Validator/Equal.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace EasySwoole\HttpAnnotation\Attributes\Validator;
+
+use EasySwoole\HttpAnnotation\Attributes\Param;
+use Psr\Http\Message\ServerRequestInterface;
+
+class Equal extends AbstractValidator
+{
+
+    private bool $strict;
+    private $compare;
+
+    function __construct($compare,bool $strict = false,string $errorMsg = null)
+    {
+        $this->compare = $compare;
+        $this->strict = $strict;
+        if(empty($errorMsg)){
+            $errorMsg = "{#name} must equal with {#compare}";
+        }
+        $this->errorMsg($errorMsg);
+    }
+
+
+    protected function validate(Param $param, ServerRequestInterface $request): bool
+    {
+        if(is_callable($this->compare)){
+            $this->compare = call_user_func($this->compare);
+        }
+        $itemData = $param->parsedValue();
+        return ($this->strict ? $itemData === $this->compare : $itemData == $this->compare);
+    }
+
+    function ruleName(): string
+    {
+        return "Equal";
+    }
+}
