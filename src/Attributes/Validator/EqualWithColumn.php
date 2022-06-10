922cfcdde3d91a2fa6c0977165780b9ec23233cc
如果的如果 <admin@fosuss.com>
2022-06-10 10:28:58 +0800

    适配

---
+++ src/Attributes/Validator/EqualWithColumn.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace EasySwoole\HttpAnnotation\Attributes\Validator;
+
+use EasySwoole\HttpAnnotation\Attributes\Param;
+use EasySwoole\HttpAnnotation\Exception\Annotation;
+use Psr\Http\Message\ServerRequestInterface;
+
+class EqualWithColumn extends AbstractValidator
+{
+
+    public $compare;
+
+    private bool $strict;
+
+    function __construct(string $compare,bool $strict = false,string $errorMsg = null)
+    {
+        $this->compare = $compare;
+        $this->strict = $strict;
+        if(empty($errorMsg)){
+            $errorMsg = "{#name} must equal with {#compare} column";
+        }
+        $this->errorMsg($errorMsg);
+    }
+
+    protected function validate(Param $param, ServerRequestInterface $request): bool
+    {
+        $itemData = $param->parsedValue();
+        $list = $this->allCheckParams();
+        if(!isset($list[$this->compare])){
+            throw new Annotation("compare param: {$this->compare} require in DifferentWithColumn rule ,but not define in any controller annotation");
+        }
+        $compare = $list[$this->compare]->parsedValue();
+
+        if($this->strict){
+            return  $itemData === $compare;
+        }else{
+            return  $itemData == $compare;
+        }
+    }
+
+    function ruleName(): string
+    {
+        return "EqualWithColumn";
+    }
+}
