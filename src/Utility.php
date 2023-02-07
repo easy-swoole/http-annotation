197346f46dda8bddea97851e2457221cb5e33f0d
如果的如果 <admin@fosuss.com>
2023-02-08 00:40:13 +0800

    新增ExtendParam

--- src/Utility.php
+++ src/Utility.php
@@ -2,13 +2,51 @@
 
 namespace EasySwoole\HttpAnnotation;
 
+use EasySwoole\HttpAnnotation\Attributes\Api;
+use EasySwoole\HttpAnnotation\Attributes\Param;
+use EasySwoole\HttpAnnotation\Exception\Annotation;
 use FastRoute\RouteCollector;
 
 class Utility
 {
-    public static function parseMethodParams(\ReflectionClass $reflectionClass,string $methodName):array
+    public static function parseMethodParams(\ReflectionClass $reflectionClass,string $methodName):?array
     {
-        return [];
+        if(!$reflectionClass->hasMethod($methodName)){
+            return null;
+        }
+        $actionMethodRef = $reflectionClass->getMethod($methodName);
+        $actionApiTags = $actionMethodRef->getAttributes(Api::class);
+        $finalParams = [];
+        if(!empty($actionApiTags)){
+            try{
+                $apiTag = new Api(...$actionApiTags[0]->getArguments());
+                /** @var Param $item */
+                foreach ($apiTag->requestParam as $item){
+                    $finalParams[$item->name] = $item;
+                }
+            }catch (\Throwable $exception){
+                $class = static::class;
+                $msg = "{$exception->getMessage()} in controller: {$reflectionClass->name} method: {$methodName}";
+                throw new Annotation($msg);
+            }
+        }else{
+            $actionParamTags = $actionMethodRef->getAttributes(Param::class);
+            foreach ($actionParamTags as $actionParamTag){
+                try{
+                    $param = new Param(...$actionParamTag->getArguments());
+                    if(!isset($finalParams[$param->name])){
+                        $finalParams[$param->name] = $param;
+                    }else{
+                        throw new Annotation("can not redefine param {$param->name}");
+                    }
+                }catch (\Throwable $exception){
+                    $msg = "{$exception->getMessage()} in controller: {$reflectionClass->name} method: {$methodName}";
+                    throw new Annotation($msg);
+                }
+            }
+        }
+
+        return $finalParams;
     }
 
     static function mappingRouter(
