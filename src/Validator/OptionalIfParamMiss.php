dfc2fc871ab6e5f7009ab6504eb5375b57a7bdb6
如果的如果 <admin@fosuss.com>
2023-02-20 23:27:42 +0800

    RequireIf和OptionalIf支持

--- src/Validator/OptionalIfParamMiss.php
+++ src/Validator/OptionalIfParamMiss.php
@@ -2,7 +2,28 @@
 
 namespace EasySwoole\HttpAnnotation\Validator;
 
-class OptionalIfParamMiss
+use EasySwoole\HttpAnnotation\Attributes\Param;
+use EasySwoole\HttpAnnotation\Validator\AbstractInterface\AbstractValidator;
+use Psr\Http\Message\ServerRequestInterface;
+
+class OptionalIfParamMiss extends AbstractValidator
 {
 
+    function __construct(string $paramName,?string $errorMsg = null)
+    {
+        if(empty($errorMsg)){
+            $errorMsg = "{#name} is optional when param {$paramName} miss";
+        }
+        $this->errorMsg($errorMsg);
+    }
+
+    protected function validate(Param $param, ServerRequestInterface $request): bool
+    {
+        // TODO: Implement validate() method.
+    }
+
+    function ruleName(): string
+    {
+        return 'OptionalIfParamMiss';
+    }
 }
