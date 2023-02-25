370b24df627561ef02b8a906e86248998631420e
如果的如果 <admin@fosuss.com>
2023-02-26 00:29:05 +0800

    文档展示优化

--- src/Attributes/Description.php
+++ src/Attributes/Description.php
@@ -5,26 +5,41 @@ namespace EasySwoole\HttpAnnotation\Attributes;
 #[\Attribute]
 class Description implements \JsonSerializable
 {
-    const PLAIN_TEXT = 1;
-    const JSON = 20;
-    const JSON_FILE = 21;
-    const XML = 30;
+    const PLAIN_TEXT = 'PLAIN_TEXT';
 
-    const XML_FILE = 31;
+    const  PLAIN_TEXT_FILE = 'PLAIN_TEXT';
 
-    function __construct(public string $desc,public int $type = Description::PLAIN_TEXT)
+    const JSON = 'JSON';
+    const JSON_FILE = 'JSON_FILE';
+    const XML = 'XML';
+
+    const XML_FILE = 'XML_FILE';
+
+    const MARKDOWN = 'MARKDOWN';
+
+    const MARKDOWN_FILE = 'MARKDOWN_FILE';
+
+    function __construct(public string $desc,public string $type = Description::PLAIN_TEXT)
     {
 
     }
 
     public function jsonSerialize(): mixed
     {
-        if($this->type == self::JSON){
+        $des = $this->desc;
+        if(in_array($this->type,[self::PLAIN_TEXT_FILE,self::JSON_FILE,self::MARKDOWN_FILE])){
+            $des = file_get_contents($this->desc);
+        }
+
+        if($this->type == self::JSON || $this->type == self::JSON_FILE){
             $des = json_decode($this->desc,true);
             $des = json_encode($des,JSON_PRETTY_PRINT);
-        }else{
-            $des = $this->desc;
         }
+
+        if($this->type == self::XML || $this->type == self::XML_FILE){
+
+        }
+
         return [
             "desc"=>$des,
             "type"=>$this->type
