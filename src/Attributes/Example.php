016e3b17cb214c5957bbdd93b90b8132b3ac745e
如果的如果 <admin@fosuss.com>
2023-03-06 11:04:10 +0800

    优化example传参自动识别

--- src/Attributes/Example.php
+++ src/Attributes/Example.php
@@ -12,9 +12,34 @@ class Example implements \JsonSerializable
     private string $exampleType = self::TYPE_PARAM_ARRAY;
 
     function __construct(
-        public array|Description $example
+        public array|Description|string $example
     ){
         if(!is_array($this->example)){
+            if(is_string($this->example)){
+                if(is_file($this->example)){
+                    $ext = explode('.',$this->example);
+                    $ext = array_pop($ext);
+                    $type = Description::PLAIN_TEXT;
+                    switch ($ext){
+                        case 'json':{
+                            $type = Description::JSON_FILE;
+                            break;
+                        }
+                        case 'md':{
+                            $type = Description::MARKDOWN_FILE;
+                            break;
+                        }
+
+                        case "xml":{
+                            $type = Description::XML_FILE;
+                            break;
+                        }
+                    }
+                    $this->example = new Description($this->example,$type);
+                }else{
+                    $this->example = new Description($this->example);
+                }
+            }
             $this->exampleType = self::TYPE_DESCRIPTION;
         }
     }
