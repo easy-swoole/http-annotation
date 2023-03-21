d746f28559a2c041b4214be1bde3bdf9d41deeb7
如果的如果 <admin@fosuss.com>
2023-03-21 23:10:06 +0800

    AlphaDash

--- src/Attributes/Description.php
+++ src/Attributes/Description.php
@@ -21,7 +21,30 @@ class Description implements \JsonSerializable
 
     function __construct(public string $desc,public string $type = Description::PLAIN_TEXT)
     {
+        //做校验
+        if($this->type ==  Description::PLAIN_TEXT){
+            if(is_file($this->desc)){
+                $ext = explode('.',$this->desc);
+                $ext = array_pop($ext);
+                $type = Description::PLAIN_TEXT;
+                switch ($ext){
+                    case 'json':{
+                        $type = Description::JSON_FILE;
+                        break;
+                    }
+                    case 'md':{
+                        $type = Description::MARKDOWN_FILE;
+                        break;
+                    }
 
+                    case "xml":{
+                        $type = Description::XML_FILE;
+                        break;
+                    }
+                }
+                $this->type = $type;
+            }
+        }
     }
 
     public function jsonSerialize(): mixed
--- src/Attributes/Example.php
+++ src/Attributes/Example.php
@@ -14,31 +14,10 @@ class Example implements \JsonSerializable
     function __construct(
         public array|Description|string $example
     ){
+
         if(!is_array($this->example)){
             if(is_string($this->example)){
-                if(is_file($this->example)){
-                    $ext = explode('.',$this->example);
-                    $ext = array_pop($ext);
-                    $type = Description::PLAIN_TEXT;
-                    switch ($ext){
-                        case 'json':{
-                            $type = Description::JSON_FILE;
-                            break;
-                        }
-                        case 'md':{
-                            $type = Description::MARKDOWN_FILE;
-                            break;
-                        }
-
-                        case "xml":{
-                            $type = Description::XML_FILE;
-                            break;
-                        }
-                    }
-                    $this->example = new Description($this->example,$type);
-                }else{
-                    $this->example = new Description($this->example);
-                }
+                $this->example = new Description($this->example);
             }
             $this->exampleType = self::TYPE_DESCRIPTION;
         }
