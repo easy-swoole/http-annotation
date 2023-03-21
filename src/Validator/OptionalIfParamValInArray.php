1127b0ea3ba9a9657be2904cf2d4b1d10ebe0032
如果的如果 <admin@fosuss.com>
2023-03-21 23:42:00 +0800

    AlphaDash

---
+++ src/Validator/OptionalIfParamValInArray.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace EasySwoole\HttpAnnotation\Validator;
+
+use EasySwoole\HttpAnnotation\Attributes\Param;
+use EasySwoole\HttpAnnotation\Validator\AbstractInterface\AbstractValidator;
+use Psr\Http\Message\ServerRequestInterface;
+
+class OptionalIfParamValInArray extends AbstractValidator
+{
+    protected string $paramName;
+
+    function __construct(string $paramName,?string $errorMsg = null)
+    {
+        $this->paramName = $paramName;
+        if(empty($errorMsg)){
+            $errorMsg = "{#name} is optional when param {$paramName} miss";
+        }
+        $this->errorMsg($errorMsg);
+    }
+    protected function validate(Param $param, ServerRequestInterface $request): bool
+    {
+        // TODO: Implement validate() method.
+    }
+
+    function ruleName(): string
+    {
+        // TODO: Implement ruleName() method.
+    }
+
+}
