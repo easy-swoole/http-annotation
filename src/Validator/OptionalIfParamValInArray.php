df79cefeeec499e5887ae1dba478bc2fb1647be2
如果的如果 <admin@fosuss.com>
2023-03-22 10:57:55 +0800

    AlphaDash

--- src/Validator/OptionalIfParamValInArray.php
+++ src/Validator/OptionalIfParamValInArray.php
@@ -9,23 +9,25 @@ use Psr\Http\Message\ServerRequestInterface;
 class OptionalIfParamValInArray extends AbstractValidator
 {
     protected string $paramName;
+    protected array $inVal;
 
-    function __construct(string $paramName,?string $errorMsg = null)
+    function __construct(string $paramName,array $inVal,?string $errorMsg = null)
     {
+        $this->inVal = $inVal;
         $this->paramName = $paramName;
         if(empty($errorMsg)){
-            $errorMsg = "{#name} is optional when param {$paramName} miss";
+            $errorMsg = "{#name} is optional when param {$paramName} value is in ".json_encode($inVal);
         }
         $this->errorMsg($errorMsg);
     }
     protected function validate(Param $param, ServerRequestInterface $request): bool
     {
-        // TODO: Implement validate() method.
+        return true;
     }
 
     function ruleName(): string
     {
-        // TODO: Implement ruleName() method.
+        return 'OptionalIfParamValInArray';
     }
 
 }
--- src/Validator/OptionalIfParamValNoInArray.php
+++ src/Validator/OptionalIfParamValNoInArray.php
@@ -2,7 +2,32 @@
 
 namespace EasySwoole\HttpAnnotation\Validator;
 
-class OptionalIfParamValNoInArray
+use EasySwoole\HttpAnnotation\Attributes\Param;
+use EasySwoole\HttpAnnotation\Validator\AbstractInterface\AbstractValidator;
+use Psr\Http\Message\ServerRequestInterface;
+
+class OptionalIfParamValNoInArray extends AbstractValidator
 {
 
+    protected string $paramName;
+    protected array $inVal;
+
+    function __construct(string $paramName,array $inVal,?string $errorMsg = null)
+    {
+        $this->inVal = $inVal;
+        $this->paramName = $paramName;
+        if(empty($errorMsg)){
+            $errorMsg = "{#name} is optional when param {$paramName} value is not in ".json_encode($inVal);
+        }
+        $this->errorMsg($errorMsg);
+    }
+    protected function validate(Param $param, ServerRequestInterface $request): bool
+    {
+        // TODO: Implement validate() method.
+    }
+
+    function ruleName(): string
+    {
+        // TODO: Implement ruleName() method.
+    }
 }
